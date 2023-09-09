3c875a296fa83af193e5b49038fd8a77d495a101
jkkCoder <jayeshk118@gmail.com>
2023-09-10 04:07:00 +0530

    playing bg video

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const VideoTitle = ({title, description}) => {
   return (
-    <div className='pt-36 px-12'>
+    <div className='w-screen aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black'>
         <h1 className='text-6xl font-bold'>{title}</h1>
         <p className='py-6 text-lg w-1/4 '>{description}</p>
         <div className=''>
-            <button className='bg-gray-500 bg-opacity-50 rounded-lg text-white p-4 px-12 text-xl'>▶ Play</button>
-            <button className='mx-2 bg-gray-500 bg-opacity-50 rounded-lg text-white p-4 px-12 text-xl'>More Info</button>
+            <button className='bg-white rounded-lg hover:opacity-80 text-black p-4 px-12 text-xl'>▶ Play</button>
+            <button className='mx-2 bg-gray-500 hover:opacity-80 bg-opacity-50 rounded-lg text-white p-4 px-12 text-xl'>More Info</button>
         </div>
     </div>
   )
